304bd691ef854ed7fa100f0ae0df9298dabda2c8
Arjen <arjen@E71187C8M240132.local>
2023-11-15 13:45:57 +0800

    fix font

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>YEP - La Liga Filipina</title>
+
+    <style>
+        @font-face {
+            font-family: 'Augusta';
+            src: url('{{ asset("fonts/Augusta-Regular.woff2") }}') format('woff2'),
+                url('{{ asset("fonts/Augusta-Regular.woff") }}') format('woff');
+            font-weight: normal;
+            font-style: normal;
+            font-display: swap;
+        }
+
+        @font-face {
+            font-family: 'Chapaza';
+            src: url('{{ asset("fonts/Chapaza-Regular.woff2") }}') format('woff2'),
+                url('{{ asset("fonts/Chapaza-Regular.woff") }}') format('woff');
+            font-weight: normal;
+            font-style: normal;
+            font-display: swap;
+        }
+    </style>
+
     @vite(['resources/css/app.css','resources/js/app.js'])
 </head>
 <body>
