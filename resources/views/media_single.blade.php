11b0b662aabe323c94335feb7d963d7b95a5482b
ryntnmsn <ryn.tnmsn@gmail.com>
2024-01-18 09:25:49 +0800

    commit

--- resources/views/media_single.blade.php
+++ resources/views/media_single.blade.php
@@ -7,9 +7,9 @@
             <img src="{{ url('storage/' . implode($media->image)) }}" alt="" class="w-full sepia">
        </div>
 
-       <div class="mt-10 flex space-x-3 flex-wrap">
+       <div class="mt-10 text-center">
         <h1 class="text-xl">Photos Link:</h1><br>
-        <a class="text-xl text-rose-600" href="{{ $media->link }}">{{ $media->link }}</a>
+        <a class="text-xl bg-rose-600 text-yellow-50 px-10 py-3" href="{{ $media->link }}">CLICK HERE</a>
        </div>
     </div>
 @endsection
